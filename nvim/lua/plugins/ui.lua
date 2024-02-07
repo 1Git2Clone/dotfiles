a50c3f8593d3b163ef2bc3804290b0a2458dc06a
1Kill2Steal <imliterallyhim@duck.com>
2024-02-07 15:40:52 +0200

    .

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -2,38 +2,38 @@ local Util = require("lazyvim.util")
 
 return {
 	-- Better `vim.notify()`
-	{
-		"rcarriga/nvim-notify",
-		keys = {
-			{
-				"<leader>un",
-				function()
-					require("notify").dismiss({ silent = true, pending = true })
-				end,
-				desc = "Dismiss all Notifications",
-			},
-		},
-		opts = {
-			timeout = 3000,
-			max_height = function()
-				return math.floor(vim.o.lines * 0.75)
-			end,
-			max_width = function()
-				return math.floor(vim.o.columns * 0.75)
-			end,
-			on_open = function(win)
-				vim.api.nvim_win_set_config(win, { zindex = 100 })
-			end,
-		},
-		init = function()
-			-- when noice is not enabled, install notify on VeryLazy
-			if not Util.has("noice.nvim") then
-				Util.on_very_lazy(function()
-					vim.notify = require("notify")
-				end)
-			end
-		end,
-	},
+	-- {
+	-- 	"rcarriga/nvim-notify",
+	-- 	keys = {
+	-- 		{
+	-- 			"<leader>un",
+	-- 			function()
+	-- 				require("notify").dismiss({ silent = true, pending = true })
+	-- 			end,
+	-- 			desc = "Dismiss all Notifications",
+	-- 		},
+	-- 	},
+	-- 	opts = {
+	-- 		timeout = 3000,
+	-- 		max_height = function()
+	-- 			return math.floor(vim.o.lines * 0.75)
+	-- 		end,
+	-- 		max_width = function()
+	-- 			return math.floor(vim.o.columns * 0.75)
+	-- 		end,
+	-- 		on_open = function(win)
+	-- 			vim.api.nvim_win_set_config(win, { zindex = 100 })
+	-- 		end,
+	-- 	},
+	-- 	init = function()
+	-- 		-- when noice is not enabled, install notify on VeryLazy
+	-- 		if not Util.has("noice.nvim") then
+	-- 			Util.on_very_lazy(function()
+	-- 				vim.notify = require("notify")
+	-- 			end)
+	-- 		end
+	-- 	end,
+	-- },
 
 	-- better vim.ui
 	{
@@ -455,13 +455,14 @@ return {
 						local stats = require("lazy").stats()
 						local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
 						return {
-							"⚡ HuTaovim loaded "
-								.. stats.loaded
-								.. "/"
-								.. stats.count
-								.. " plugins in "
+							"🔥 Hu Tao #1! "
+								-- .. stats.loaded
+								-- .. "/"
+								-- .. stats.count
+								-- .. " plugins in "
+								.. "("
 								.. ms
-								.. "ms",
+								.. "ms) 🔥",
 						}
 					end,
 				},
