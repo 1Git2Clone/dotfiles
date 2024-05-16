c90e0c1149675e1a292b8abfc4a430c810676bad
1Kill2Steal <ivankirilov640@gmail.com>
2024-05-16 16:36:33 +0300

    Removed old config files

--- programs/bin/git-auto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd . # work on the current dir.
-
-git add .
-git commit -m "."
-git fetch
-git pull
-git push
--- programs/bin/start-ssh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ./
-
-eval $(ssh-agent)
-ssh-add ~/.ssh/id_rsa
-ssh-add -l
