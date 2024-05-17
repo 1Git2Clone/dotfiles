473fa8493c76a33d13021ad4732e66b645ddf2bb
1Kill2Steal <ivankirilov640@gmail.com>
2024-05-17 15:44:30 +0300

    Added a bulk color shift script

---
+++ bulk_color_shift.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+color_shift ./config/starship.toml ./config/starship.toml $1
+color_shift ./config/HyprV/kitty/mocha.conf ./config/HyprV/kitty/mocha.conf $1
+color_shift ./config/nvim/lua/plugins/colorscheme.lua ./config/nvim/lua/plugins/colorscheme.lua $1
