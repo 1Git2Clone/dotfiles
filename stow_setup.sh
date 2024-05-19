5e12f990f24c2b6052f3438384d5aed75287e08e
1Kill2Steal <51541614+1Kill2Steal@users.noreply.github.com>
2024-05-19 14:58:53 +0300

    Updated stow script and added some obs plugins

--- stow_setup.sh
+++ stow_setup.sh
@@ -1,5 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
-stow home_user/ -t "$HOME/"
-stow config/ -t "$HOME/.config/"
-stow cargo/ -t "$HOME/.cargo/"
+if ! command -v stow >/dev/null 2>&1; then
+	echo "You need to have GNU stow installed."
+	exit 1
+fi
+
+stow --adopt -R home_user/ -t "$HOME/"
+
+mkdir -p "$HOME/.config/obs-studio/plugin_config/advanced-scene-switcher/"
+cd ./obs_plugins/
+stow --adopt -R advanced-scene-switcher/ -t "$HOME/.config/obs-studio/plugin_config/advanced-scene-switcher/"
+cd ../
+stow --adopt config/ -t "$HOME/.config/"
+
+mkdir -p "$HOME/.cargo/"
+stow --adopt -R cargo/ -t "$HOME/.cargo/"
