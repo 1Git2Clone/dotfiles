443fb990111f7ca6916a9716948b2eeedcd93e27
1Kill2Steal <51541614+1Kill2Steal@users.noreply.github.com>
2024-05-21 13:31:48 +0300

    Added --dotfiles flags

--- stow_setup.sh
+++ stow_setup.sh
@@ -5,13 +5,15 @@ if ! command -v stow >/dev/null 2>&1; then
 	exit 1
 fi
 
-stow --adopt -R home_user/ -t "$HOME/"
+cd ~/dotfiles
+
+stow --adopt --dotfiles home_user/ -t "$HOME/"
 
 mkdir -p "$HOME/.config/obs-studio/plugin_config/advanced-scene-switcher/"
 cd ./obs_plugins/
-stow --adopt -R advanced-scene-switcher/ -t "$HOME/.config/obs-studio/plugin_config/advanced-scene-switcher/"
+stow --adopt advanced-scene-switcher/ -t "$HOME/.config/obs-studio/plugin_config/advanced-scene-switcher/"
 cd ../
-stow --adopt config/ -t "$HOME/.config/"
+stow --adopt --dotfiles config/ -t "$HOME/.config/"
 
 mkdir -p "$HOME/.cargo/"
-stow --adopt -R cargo/ -t "$HOME/.cargo/"
+stow --adopt --dotfiles cargo/ -t "$HOME/.cargo/"
