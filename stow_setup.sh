026afb3c65caed057bd6133a071f10075381cd09
1Kill2Steal <51541614+1Kill2Steal@users.noreply.github.com>
2024-05-21 13:49:10 +0300

    Changed the stow setup to a one liner

--- stow_setup.sh
+++ stow_setup.sh
@@ -6,14 +6,4 @@ if ! command -v stow >/dev/null 2>&1; then
 fi
 
 cd ~/dotfiles
-
-stow --adopt --dotfiles home_user/ -t "$HOME/"
-
-mkdir -p "$HOME/.config/obs-studio/plugin_config/advanced-scene-switcher/"
-cd ./obs_plugins/
-stow --adopt advanced-scene-switcher/ -t "$HOME/.config/obs-studio/plugin_config/advanced-scene-switcher/"
-cd ../
-stow --adopt --dotfiles config/ -t "$HOME/.config/"
-
-mkdir -p "$HOME/.cargo/"
-stow --adopt --dotfiles cargo/ -t "$HOME/.cargo/"
+stow --adopt --dotfiles . -t "$HOME/"
