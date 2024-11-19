a6b835d352d3d4212451f5c81c726217ef5df458
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2024-11-19 09:56:50 +0200

    Small script changes

--- stow_setup.sh
+++ stow_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if ! command -v stow >/dev/null 2>&1; then
-	echo "You need to have GNU stow installed."
-	exit 1
+  echo "You need to have GNU stow installed."
+  exit 1
 fi
 
-cd ~/dotfiles
+cd ~/dotfiles || echo "You should have a $HOME/dotfiles/ directory." && exit 1
 stow --adopt --dotfiles . -t "$HOME/"
