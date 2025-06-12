bee7b4eabd143f7282c20694fda1c4956adc5124
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2025-06-12 16:17:44 +0300

    Quoted entry

--- dot-config/programs/shell_scripts/cliphist-remove-entry.sh
+++ dot-config/programs/shell_scripts/cliphist-remove-entry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 entry=$(cliphist list | wofi --dmenu --prompt "Pick an entry to remove")
-if [[ -n $entry ]]; then
+if [[ -n "$entry" ]]; then
   echo "$entry" | cliphist delete
   notify-send --app-name "cliphist-remove-entry.sh" "✅ Removed entry" "$entry"
 fi
